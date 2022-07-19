12e97b550a13f7c11c85082f2028923e2162b7d8
santihacker18 <sboterovalencia@gmail.com>
2022-07-19 18:41:37 -0500

    subo tarea martes

---
+++ santiago/documentacion/tareaMartes.sql
@@ -0,0 +1,6 @@
+select (s.name ||' '|| lastname1 ||' '|| lastname2) as fullname,
+count (sc.id_class) as clases
+from students as s 
+left join students_classes as sc on s.id = sc.id_student
+where sc.id_class != 0
+group by fullname

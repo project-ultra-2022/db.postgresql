6b3474952b261a0874f35f4312e1214a67acff75
santihacker18 <sboterovalencia@gmail.com>
2022-07-19 21:32:15 -0500

    correccion tarea

--- santiago/documentacion/tareaMartes.sql
+++ santiago/documentacion/tareaMartes.sql
@@ -3,4 +3,4 @@ count (sc.id_class) as clases
 from students as s 
 left join students_classes as sc on s.id = sc.id_student
 where sc.id_class != 0
-group by fullname
+group by (s.name ||' '|| lastname1 ||' '|| lastname2)

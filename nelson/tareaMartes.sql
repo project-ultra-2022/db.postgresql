f214025458d724dd5e3292a051a0287cf5e442b8
NelsonBotero1518 <nelsonkennedy2004@gmail.com>
2022-07-19 20:57:38 -0500

    correcion tarea martes

--- nelson/tareaMartes.sql
+++ nelson/tareaMartes.sql
@@ -1,6 +1,6 @@
 select (s.name ||' '|| s.lastname1 ||' '|| s.lastname2) as fullname,
-count (sc.id_class) as clases_asignadas
+count (sc.id_class) 
 from students as s
 left join students_classes as sc on s.id = sc.id_student
-group by fullname
-order by clases_asignadas desc
+group by  (s.name ||' '|| s.lastname1 ||' '|| s.lastname2) 
+order by count (sc.id_class) desc
